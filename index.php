506238e7299cb322912e939e3762c3d9794672b5
Kevin Castellon <juniorcasled@campus.monlau.com>
2025-09-25 18:59:06 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página PHP</title>
+</head>
+<body>
+    <?php
+    // Tu código PHP aquí
+    echo "<h1>¡Bienvenido a mi página Sarsam gay!</h1>";
+    ?>
+</body>
+</html>
